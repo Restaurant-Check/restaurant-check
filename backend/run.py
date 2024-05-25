1faa7631ccba6fc69b40c6324d553a08ac737eb3
u200b <@>
2024-05-25 16:27:17 +0200

    fix: remove cors

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,9 @@
 from app import create_app
+from flask_cors import CORS
 
 app = create_app()
 
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
